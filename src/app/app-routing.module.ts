5c465519299d36c81d7a428d691939f5e4fb6179
Rodrigo Pozo <rodrigo.pozo25@gmail.com>
2023-09-11 19:38:55 -0500

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PokeTableComponent} from "./components/poke-table/poke-table.component";
+import {PokeDetailComponent} from "./components/poke-detail/poke-detail.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: PokeTableComponent },
+  { path: 'pokemon/:id', component: PokeDetailComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
